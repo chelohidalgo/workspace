81dd0bc346ffc5c867cb87555183f769d45ac955
Marcelo Nisum <marcelonisum@Marcelos-MacBook-Pro.local>
2022-01-19 17:58:52 -0500

    springboot test + junit tests

---
+++ demo-test/src/test/java/com/hidalgomarcelo/demotest/HttpRequestTest.java
@@ -0,0 +1,23 @@
+package com.hidalgomarcelo.demotest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws  Exception {
+        Assertions.assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class)).isEqualTo("Hello, World");
+    }
+}
