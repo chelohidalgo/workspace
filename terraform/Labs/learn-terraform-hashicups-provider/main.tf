425a522fd084e62225695aff83aef233b92fc849
Marcelo Nisum <marcelonisum@Marcelos-MacBook-Pro.local>
2022-05-20 09:16:19 -0500

    add terraform examples and practices

---
+++ terraform/Labs/learn-terraform-hashicups-provider/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    hashicups = {
+      version = "~> 0.3.1"
+      source  = "hashicorp.com/edu/hashicups"
+    }
+  }
+}
+
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+}
+
+resource "hashicups_order" "edu" {
+  items {
+    coffee {
+      id = 3
+    }
+    quantity = 3
+  }
+  items {
+    coffee {
+      id = 2
+    }
+    quantity = 1
+  }
+}
+
+output "edu_order" {
+  value = hashicups_order.edu
+}
+
+data "hashicups_ingredients" "first_coffee" {
+  coffee_id = hashicups_order.edu.items[0].coffee[0].id
+}
+
+output "first_coffee_ingredients" {
+  value = data.hashicups_ingredients.first_coffee
+}
+
+
---
+++ terraform/Labs/learn-terraform-locals/README.md
@@ -0,0 +1,6 @@
+# Learn Terraform locals
+
+Terraform locals allow you to simplify your Terraform configuration and avoid
+repetition. Local values can also help you write more readable configuration by
+using meaningful names rather than hard-coding values. Follow along with [this
+tutorial](https://learn.hashicorp.com/tutorials/terraform/locals?in=terraform/configuration-language) on HashiCorp Learn.
